85d71e38e0e0dae9b78e54d3999ce396f53faf8d
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-08-22 17:40:22 +0200

    opened a method that has a menu

--- src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
+++ src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
@@ -60,8 +60,70 @@ public class MediCareHospitall {
             case 11-> System.out.println("Exiting system. Goodbye!");
             default -> System.out.println("Invalid option selected.");         
 
+        }   
+    }
+    private static void registerPatient(){
+     
+        System.out.println("Enter patient ID");
+        String id=scan.nextLine();
+        
+        System.out.println("Enter first name");
+        String name=scan.nextLine();
+        
+        System.out.println("Enter last name");
+        String surname=scan.nextLine();
+        
+        System.out.println("Enter age");
+        int age=scan.nextInt();
+        
+        System.out.println("Enter gender");
+        String gender=scan.nextLine();
+        
+        System.out.println("Enter medical condition");
+        String condition=scan.nextLine();
+        
+        System.out.println("Select Category: 1.Inpatient 2.Outpatient 3.Emergency");
+        int choice=Integer.parseInt(scan.nextLine());
+        Patient patient = null;
+        
+        if(ward.registerPatient(patient)){
+            System.out.println("Patient registered successfully");
+        }else{
+            System.out.println("Patient ID already exists");
         }
     }
-}
-            
-
+    private static void searchPatient(){
+      System.out.print("Enter Patient ID to search: ");
+        String id = scan.nextLine();
+        Patient p = ward.findPatientbyId(id);
+        if (p != null) {
+            System.out.println(p.printDetails());
+        } else {
+            System.out.println("Patient not found.");
+        }
+    }
+    private static void updatePatient(){
+        
+        System.out.print("Enter Patient ID to update: ");
+        String id = scan.nextLine();
+        
+        System.out.print("New First Name: ");
+        String fname = scan.nextLine();
+        System.out.print("New Last Name: ");
+        String lname = scan.nextLine();
+        System.out.print("New Age: ");
+        int age = Integer.parseInt(scan.nextLine());
+        System.out.print("New Gender: ");
+        String gender = scan.nextLine();
+        System.out.print("New Medical Condition: ");
+        String condition=scan.nextLine();
+        
+        if(ward.updatePatientDetails(gender, fname, lname, age, gender, condition)){
+            System.out.println("Patient details updated successfully.");
+        }
+    }
+    public static void deletePatient(){
+        System.out.print("Enter Patient ID to delete: ");
+        String id = scan.nextLine();
+    }
+    }
