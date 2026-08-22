565681ddf34bdb0843150bf3a753c9130feedea8
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-08-22 15:48:40 +0200

    declared the bed array and created other methods

--- src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
+++ src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
@@ -3,13 +3,16 @@
  */
 
 package com.mycompany.medicarehospitall;
-
+import java.util.Scanner;
 /**
  *
  * @author Tshireletso
  */
 public class MediCareHospitall {
 
+    private static wardManagement ward=new wardManagement();
+    private static Scanner scan=new Scanner(System.in);
+    
     public static void main(String[] args) {
         
     }
