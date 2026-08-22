1229b373152beb9dd1aa0e2bebd593143064677b
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-08-22 16:02:16 +0200

    aadded scanner on main class

--- src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
+++ src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
@@ -14,6 +14,40 @@ public class MediCareHospitall {
     private static Scanner scan=new Scanner(System.in);
     
     public static void main(String[] args) {
+       int option=0;
+       
+       do{
+           System.out.println("\n==========================================");
+            System.out.println("  MediCare Hospital Admission System");
+            System.out.println("==========================================");
+            System.out.println("1. Register New Patient");
+            System.out.println("2. Search Patient by ID");
+            System.out.println("3. Update Patient Details");
+            System.out.println("4. Delete Patient");
+            System.out.println("5. Display All Patients");
+            System.out.println("6. Display Ward Layout");
+            System.out.println("7. Allocate Bed to Inpatient");
+            System.out.println("8. Release Bed");
+            System.out.println("9. Generate Ward Reports");
+            System.out.println("10. Sort Patients");
+            System.out.println("11. Exit");
+            System.out.print("Select an option 1-11 ");
+            
+            option=scan.nextInt();
+            
+            if(scan.hasNextInt()){
+                handleOption(option);
+            }else{
+                System.out.println("Invalid input! Please enter a number.");
+      
+            }
+            
+       }while (option !=11);
+    }
+    private static void handleOption(int option){
+        
         
     }
 }
+            
+
