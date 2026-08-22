e4a63abd2f0140d796dc6d17619ac92b6798f138
Tshireletso <Tshireletso@WIN-KH2FCB95OQ6.lan>
2026-08-22 13:36:21 +0200

    opened a enum class PatientCategory

---
+++ src/main/java/com/mycompany/medicarehospitall/MediCareHospitall.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.medicarehospitall;
+
+/**
+ *
+ * @author Tshireletso
+ */
+public class MediCareHospitall {
+
+    public static void main(String[] args) {
+        
+    }
+}
